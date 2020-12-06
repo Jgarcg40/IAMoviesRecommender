2ecaa47ee3c565c395e9f7e8ce3e6855ce53bb80
Jgarcg40 <Jgarcg40@estudiantes.unileon.es>
2020-12-06 02:49:33 +0100

    hecho el store del frontend

---
+++ AppFrontend/src/store/index.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  getters: {
+    getMovies: state => state.movies
+  },
+
+  state: {
+    movies: []
+  },
+  mutations: {
+    update_movies(state, movies) {
+      state.movies = movies;
+    }
+  },
+  actions: {
+    sendParams({ commit }, params) {
+      return new Promise((resolve, reject) => {
+        axios({
+          url: "http://localhost:3000/getMovies",
+          data: params,
+          method: "POST"
+        })
+          .then(resp => {
+            const videogames = resp.data.movies;
+            commit("update_movies", movies);
+            alert(movies);
+            resolve(resp);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    }
+  },
+  modules: {}
+});
